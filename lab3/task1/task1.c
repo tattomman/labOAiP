51e37721a59fa8283f9a7a847725194c0f84b43b
tattomman <andreilolovich@gmail.com>
2022-10-13 23:18:23 +0300

    task1 edited

--- lab3/task1/task1.c
+++ lab3/task1/task1.c
@@ -1,8 +1,41 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+float sum_of_odd(int amt_of_elem, float array[])
+{
+	float sum_of_odd = 0;
+	for (int i = 0; i < amt_of_elem; i++)
+	{
+		if (i % 2 != 0)
+			sum_of_odd += array[i];
+	}
+	printf("\nSum of odd elements = %f\n", sum_of_odd);
+}
+float sum_btwn_ngtv(int amt_of_elem, float array[])
+{
+	float sum_btwn_ngtv = 0;
+	int a = 0, b = 0;
+	for (int i = 0; i < amt_of_elem; i++)
+	{
+		if (array[i] < 0)
+		{
+			a = i;
+			for (int j = i; j < amt_of_elem; j++)
+			{
+				if (array[j] < 0)
+					b = j;
+			}
+			break;
+		}
+	}
+
+	for (int i = ++a; i < b; i++)
+		sum_btwn_ngtv += array[i];
+	printf("Sum of elements between first and last negative = %f\n", sum_btwn_ngtv);
+}
 int main()
 {
-	float array[100], sum_of_odd = 0, sum_btwn_ngtv = 0;
+	float array[100];
 	int n; //n - amount of array elements
 
 	printf("Enter amount of values\n");
@@ -24,32 +57,13 @@ int main()
 		}
 	}
 
+	printf("Your array: ");
 	for (int i = 0; i < n; i++)
-	{
-		if (i % 2 != 0)
-			sum_of_odd += array[i];
-	}
-	printf("Sum of odd elements = %f\n", sum_of_odd);
-
-	int a = 0, b = 0;
-	for (int i = 0; i < n; i++)
-	{
-		if (array[i] < 0)
-		{
-			a = i;
-			for (int j = i; j < n; j++)
-			{
-				if (array[j] < 0)
-					b = j;
-			}
-			break;
-		}
-	}
-
-	for (int i = ++a; i < b; i++)
-		sum_btwn_ngtv += array[i];
-	printf("Sum of elements between first and last negative = %f\n", sum_btwn_ngtv);
-
+		printf("|%.3f| ", array[i]);
+	
+	sum_of_odd(n, array);
+	sum_btwn_ngtv(n, array);
+	
 	return 0;
 }
 
